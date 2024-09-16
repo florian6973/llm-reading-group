71c20df1025fdad78274a5fac3e34f24c4a3f2f9
Florent Pollet <flo1.raymond@gmail.com>
2024-09-16 16:35:01 -0400

    Session 01: add inference

--- Session_01_Intro_Transformers/eval.py
+++ Session_01_Intro_Transformers/eval.py
@@ -1,1 +1,17 @@
 # please run train.py before eval
+
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from data import GPTConfig, decode
+from model import GPT
+
+path = 'trained_models/ckpt.pt'
+model = GPT(GPTConfig(vocab_size=66))
+model.load_state_dict(torch.load(path)['model'])
+model.eval()
+
+# after 10 min of training
+gen = model.generate(torch.LongTensor([[0]]), 1000)
+for i in range(len(gen)):
+    print(decode(gen[i].tolist()))
